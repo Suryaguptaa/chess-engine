db8dca1dba92a084da09754585b35805618d4a4b
Surya Dev Gupta <suryagupta.ws@gmail.com>
2026-02-06 16:02:42 +0530

    Cell.java and Position.java commits

--- src/main/java/com/surya/chess/domain/board/Cell.java
+++ src/main/java/com/surya/chess/domain/board/Cell.java
@@ -1,4 +1,47 @@
 package com.surya.chess.domain.board;
 
+import com.surya.chess.domain.common.Position;
+import com.surya.chess.domain.piece.Piece;
+
+import java.util.Optional;
+
 public class Cell {
+
+    private final Position position;
+    private Piece piece;
+
+    public Cell(Position position){
+        this.position = position;
+    }
+
+    public Position getPosition(){
+        return position;
+    }
+
+    public Optional<Piece> getPiece(){
+        return Optional.ofNullable(piece);
+    }
+
+    public boolean isEmpty(){
+        return piece == null;
+    }
+
+    void placePiece(Piece piece){
+        if(this.piece != null){
+            throw new IllegalStateException(
+                    "Cell " + position + " is already occupied"
+            );
+        }
+
+        this.piece = piece;
+    }
+
+    void removePiece(){
+        if(this.piece == null){
+            throw new IllegalStateException(
+                    "Cell " + position + " is already empty"
+            );
+        }
+        this.piece = null;
+    }
 }
--- src/main/java/com/surya/chess/domain/common/Position.java
+++ src/main/java/com/surya/chess/domain/common/Position.java
@@ -1,4 +1,50 @@
 package com.surya.chess.domain.common;
 
-public class Position {
+import java.util.Objects;
+
+
+public final class Position {
+
+    private final int row;
+    private final int col;
+
+    private Position(int row, int col){
+        this.row = row;
+        this.col = col;
+    }
+
+    public static Position of(int row, int col) {
+        if(row<0 || row>=8 || col <0 || col >=8 ){
+            throw new IllegalArgumentException(
+                    "Position must be within 0-7 for both row and column"
+                    );
+        }
+        return new Position(row, col);
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    public int getCol() {
+        return col;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if(this ==o) return true;
+        if(!(o instanceof Position)) return false;
+        Position position = (Position) o;
+        return row == position.row && col == position.col;
+    }
+
+    @Override
+    public int hashCode(){
+        return Objects.hash(row,col);
+    }
+
+    @Override
+    public String toString(){
+        return "(" + row + "," + col + ")";
+    }
 }
